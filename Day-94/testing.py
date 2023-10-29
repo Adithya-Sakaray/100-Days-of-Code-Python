bb8a436776c66309b2164c3f5c9ae08920c0f48f
Adithya Sakaray <adithyasakaray@gmail.com>
2023-10-29 21:59:56 +0530

    Small tweaks

--- Day-94/testing.py
+++ Day-94/testing.py
@@ -9,6 +9,8 @@ pyautogui.dragTo(550, 625, 1)
 pyautogui.dragTo(600, 625, 1)
 pyautogui.dragTo(600, 700, 1)
 pyautogui.dragTo(550, 700, 1)
+pyautogui.dragTo(550, 700, 1)
+
 
 
 print(screenWidth, screenHeight)
